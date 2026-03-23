e5784b811b471b676e6fe206d46b1bff6bd18b43
Park Seoyeon <parkseoyeon1001@gmail.com>
2026-03-23 18:35:59 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 216 ms, Memory: 2728 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.cc
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 
 
-void turn_matrix(int* Matrix,int* temp,int c_num,int N,int M, int* term);
+void turn_matrix(int* Matrix,int c_num,int N,int M);
 
 
 int main()
@@ -17,6 +16,13 @@ int main()
 
 	Matrix = (int*)malloc(N * M * sizeof(int));
 
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> Matrix[i * M + j];
+		}
+	}
 
 	int min;
 	if (N < M)
@@ -26,46 +32,10 @@ int main()
 
 	c_num = min / 2;
 
-	int a, b;
-	int count = 0;
-	int* term;
-	term = (int*)malloc(c_num * sizeof(int));
-
-	a = M - 1;
-	b = N - 1;
-
-	for (int i = 0; i < c_num; i++)
+	for (int i = 0; i < R; i++)
 	{
-		if (i == 0)
-		{
-			term[i] = (2 * a) + (2 * b);
-			term[i] = R % term[i];
-		}
-			
-		else
-		{
-			a = a - 2;
-			b = b - 2;
-			term[i] = (2 * a) + (2 * b);
-			term[i] = R % term[i];
-		}
+		turn_matrix(Matrix, c_num, N, M);
 	}
-
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			cin >> Matrix[i * M + j];
-		}
-	}
-
-	int* temp;
-	temp = (int*)malloc(N * M * sizeof(int));
-	copy(Matrix, Matrix + N * M, temp);
-
-
-	turn_matrix(Matrix, temp, c_num, N, M, term);
 	
 
 	for (int i = 0; i < N; i++)
@@ -79,53 +49,42 @@ int main()
 		cout << '\n';
 	}
 	
-	free(temp);
 	free(Matrix);
-	free(term);
 }
 
 
 
 
-void turn_matrix(int* Matrix,int* temp,int c_num,int N,int M, int* term)
+void turn_matrix(int* Matrix,int c_num,int N,int M)
 {
-	
-	
-	
+	int* temp;
+	temp= (int*)malloc(N * M * sizeof(int));
 	for (int a = 0; a < c_num; a++) //left
 	{
-		
-		for (int b = 0; b < term[a]; b++)
+		for (int j = 1 + a; j < M - a; j++)
 		{
-			for (int j = 1 + a; j < M - a; j++)
-			{
-				temp[a * M + j - 1] = Matrix[a * M + j];
-			}
-
-			for (int i = a; i < N - a - 1; i++) //down
-			{
-				temp[(i + 1) * M + a] = Matrix[i * M + a];
-			}
-
-			int k = N - 1 - a;
-			for (int j = a; j < M - a - 1; j++) //right
-			{
-				temp[k * M + j + 1] = Matrix[k * M + j];
-			}
-			k = M - 1 - a;
-			for (int i = N - 1 - a; i > a; i--) //up
-			{
-				temp[(i - 1) * M + k] = Matrix[i * M + k];
-			}
-
-			memcpy(Matrix, temp, N * M * sizeof(int));
-
+			temp[a * M + j - 1] = Matrix[a * M + j];
 		}
 
-		
+		for (int i = a; i<N-a-1; i++) //down
+		{
+			temp[(i+1)*M+a] = Matrix[i*M+a];
+		}
+
+		int k = N - 1 - a;
+		for (int j = a; j < M - a - 1; j++) //right
+		{
+			temp[k * M + j + 1] = Matrix[k * M + j];
+		}
+		k = M - 1 - a;
+		for (int i = N - 1 - a; i >a; i--) //up
+		{
+			temp[(i - 1) * M + k] = Matrix[i * M + k];
+		}
 	}
 
 
-	
-	
+	copy(temp,temp+N*M,Matrix);
+
+	free(temp);
 }
