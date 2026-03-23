af9a75efe11d98a6f2904d477f57beffe30f2550
Park Seoyeon <parkseoyeon1001@gmail.com>
2026-03-23 18:35:30 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,32 +1,90 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
-int N, M;
-int ans[10];
-bool visit[10];
 
-void dfs(int depth,int num)
-{
-	if (depth == M)
-	{
-		for (int i = 0; i < M; i++) printf("%d ", ans[i]);
-		printf("\n");
-		return;
-	}
-
-	for (int i = num; i <= N; i++)
-	{
-		if (visit[i]) continue;
-		ans[depth] = i;
-		visit[i] = true;
-		num = i+1;
-		dfs(depth + 1,num);
-		visit[i] = false;
-	}
-}
+void search(int depth, int M, int N, int* flag, int* arr,int prv);
+int total = 0;
+int MAX=1;
 
 int main()
 {
-	scanf("%d %d", &N,&M);
-	dfs(0,1);
+	int N, M;
+	
+	cin >> N >> M;
+
+	int* flag;
+	flag = (int*)malloc((N + 1) * sizeof(int));
+
+	int* arr;
+	arr = (int*)malloc(M * sizeof(int));
+
+	int a=0;
+	for (int i = N; i > 0; i--)
+	{
+		if (a >=M)
+			break;
+		MAX = MAX * i;
+		a++;
+	}
+
+	
+	for (int i = M; i > 0; i--)
+	{
+		MAX = MAX / i;
+	}
+
+
+
+	for (int i = 0; i < N + 1; i++)
+	{
+		flag[i] = 0;
+	}
+
+
+
+	int depth = 0;
+	int prv = 1;
+	search(depth, M, N, flag, arr,prv);
+	
+
+	free(flag);
+	free(arr);
+
+
+	
+
+
+}
+
+
+
+void search(int depth,int M,int N,int * flag,int* arr,int prv)
+{
+	if (depth == M)
+	{
+		total++;
+		for (int j = 0; j < M-1; j++)
+		{
+			printf("%d ", arr[j]);
+		}
+		printf("%d", arr[M-1]);
+		if (MAX > total)
+		{
+			printf("\n");
+		}
+		return;
+	}
+
+	for (int i = prv; i <= N; i++)
+	{
+		if (flag[i]==0)
+		{
+			flag[i] = 1;
+			arr[depth] = i;
+			prv = i;
+			search(depth + 1,M,N,flag,arr,prv);
+			flag[i] = 0;
+		}
+	}
 }
