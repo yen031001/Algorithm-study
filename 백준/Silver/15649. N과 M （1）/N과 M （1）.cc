5e089d2100a784ca75c6a4887afffc081f92ecdf
Park Seoyeon <parkseoyeon1001@gmail.com>
2026-03-23 18:35:33 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+void search(int depth, int M, int N, int* flag, int* arr);
+int total = 0;
+int MAX=1;
+
+int main()
+{
+	int N, M;
+	
+	cin >> N >> M;
+
+	int* flag;
+	flag = (int*)malloc((N + 1) * sizeof(int));
+
+	int* arr;
+	arr = (int*)malloc(M * sizeof(int));
+
+	int a=0;
+	for (int i = N; i > 0; i--)
+	{
+		if (a >=M)
+			break;
+		MAX = MAX * i;
+		a++;
+	}
+
+	for (int i = 0; i < N + 1; i++)
+	{
+		flag[i] = 0;
+	}
+
+
+
+	int depth = 0;
+
+	search(depth, M, N, flag, arr);
+	
+
+	free(flag);
+	free(arr);
+
+
+	
+
+
+}
+
+
+
+void search(int depth,int M,int N,int * flag,int* arr)
+{
+	if (depth == M)
+	{
+		total++;
+		for (int j = 0; j < M-1; j++)
+		{
+			printf("%d ", arr[j]);
+		}
+		printf("%d", arr[M-1]);
+		if (MAX > total)
+		{
+			printf("\n");
+		}
+		return;
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (flag[i]==0)
+		{
+			flag[i] = 1;
+			arr[depth] = i;
+			search(depth + 1,M,N,flag,arr);
+			flag[i] = 0;
+		}
+	}
+}
+
